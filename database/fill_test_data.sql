b5486f6bffff77216ac7477125a1acc668976058
Michael Doyle <mcdoyle7227@gmail.com>
2019-08-17 17:39:39 -0400

    Update database scripts

--- database/fill_test_data.sql
+++
@@ -1,6 +0,0 @@
-/* Insert users */
-INSERT INTO cca.users (user_id) VALUES (108369316512030720); /* MimiKe */
-INSERT INTO cca.users (user_id) VALUES (240904681981870084); /* Veer */
-INSERT INTO cca.users (user_id) VALUES (276592289034403841); /* Cydozet */
-
-/* Insert Schools */
